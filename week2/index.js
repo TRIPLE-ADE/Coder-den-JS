d53ef94ec3d014d07a8a7545281317f4cc58f6cf
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2022-10-15 16:46:29 +0100

    updating

--- week2/index.js
+++ week2/index.js
@@ -148,6 +148,9 @@ let num3 = Math.floor(Math.random() * 256);
 console.log(num3)
 
 // task 10
+// 
+let strIndex = Javascript;
+console.log(strIndex.charAt(Math.floor(Math.random() * 10)))
 
 // task 11
 // printing to console and escape character to move to next line
@@ -162,6 +165,31 @@ let sliceOut = "You cannot end a sentence with because because because is a conj
 console.log(sliceOut.substr(31, 23 ))
 
 
+
+//Exercise 3
+// task 1
+// counting the number of the word love in the string
+let countLove = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+console.log(countLove.match(/love/gi));
+
+// task 2
+// using match to check for all the word because in the string
+let matchBecause ='You cannot end a sentence with because because because is a conjunction'
+console.log(matchBecause.match(/because/gi))
+
+// task 3
+// 
+const sentence = "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+console.log(sentence.replace(/%/gi));
+
+// task 4
+// calculate the total income from the string
+
+let monthlyIncome = 5000;
+let annualBonus = 10000;
+let coursesPerMonth = 15000;
+
+console.log(monthlyIncome + annualBonus + coursesPerMonth + " euro");
 
 
 
